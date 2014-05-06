0b711c0108ac4cae8879e64a51342cc0b4dfa115
Daniel García <fillito@gmail.com>
2014-05-06 19:30:05 +0200

    gray footer background color

--- Demo/PDKTCollectionViewWaterfallLayoutDemo/CollectionViewManager.m
+++ Demo/PDKTCollectionViewWaterfallLayoutDemo/CollectionViewManager.m
@@ -108,7 +108,7 @@ static NSUInteger const kNumberItemsPerSection = 10;
     NSString *sectionFooterTitle=[NSString stringWithFormat:@"Section Footer %d",indexPath.section];
     sectionFooterView.titleLabel.text=sectionFooterTitle;
     sectionFooterView.titleLabel.textColor=[UIColor whiteColor];
-    sectionFooterView.backgroundColor=[UIColor blackColor];
+    sectionFooterView.backgroundColor=[UIColor grayColor];
     return sectionFooterView;
 }
 
